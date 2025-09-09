14158f5a0b750919a2f80c50d2e60ac263232d4c
Alireza <alirezatt705@gmail.com>
2025-09-10 03:28:58 +0330

    from railway to render server

--- dist/routes/contactInfo.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const contactInfo_controller_1 = require("../controllers/contactInfo.controller");
-const jwtAuth_1 = require("../utils/jwtAuth");
-const router = (0, express_1.Router)();
-router.get("/", contactInfo_controller_1.getContactInfo);
-router.put("/", jwtAuth_1.jwtAuth, contactInfo_controller_1.updateContactInfo);
-exports.default = router;
--- dist/routes/experiences.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const experiences_controller_1 = require("../controllers/experiences.controller");
-const jwtAuth_1 = require("../utils/jwtAuth");
-const router = (0, express_1.Router)();
-router.get("/", experiences_controller_1.getExperiences);
-router.put("/", jwtAuth_1.jwtAuth, experiences_controller_1.updateExperiences);
-exports.default = router;
