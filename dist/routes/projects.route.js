1248b601c000a9b09c4890373648da7a9559f656
Alireza <alirezatt705@gmail.com>
2025-09-10 03:07:00 +0330

    from railway to render

--- dist/routes/projects.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const projects_controller_1 = require("../controllers/projects.controller");
-const jwtAuth_1 = require("../utils/jwtAuth");
-const router = (0, express_1.Router)();
-router.get("/", projects_controller_1.getProjects);
-router.put("/", jwtAuth_1.jwtAuth, projects_controller_1.updateProject);
-exports.default = router;
