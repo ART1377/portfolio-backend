14312de80f911a8c9cf58b9adff21b0669e7420d
Alireza <alirezatt705@gmail.com>
2025-09-10 19:40:45 +0330

    add different name to pdf

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -3,7 +3,6 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 import cloudinary from "../utils/cloudinary";
 
-
 const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => {
@@ -11,13 +10,12 @@ const storage = new CloudinaryStorage({
     return {
       folder: "portfolio_uploads",
       resource_type: isPdf ? "raw" : "auto",
-      public_id: `${file.fieldname}-${Date.now()}-${Math.round(
-        Math.random() * 1e9
-      )}`,
+      public_id: isPdf
+        ? `Alireza Tahavori-${Date.now()}` // fixed name for PDFs
+        : `${file.fieldname}-${Date.now()}-${Math.round(Math.random() * 1e9)}`,
     };
   },
 });
-
 
 export const upload = multer({
   storage,
